14512e66750d20c9ec35db81b5da588bcf55d5c1
saamirkc <samir.kc.a20@gmail.com>
2026-08-21 16:02:32 +0545

    fix: change frontend end point in cors

--- src/main/java/com/openai/openai/config/CorsConfig.java
+++ src/main/java/com/openai/openai/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(" https://pdf-frontend-plcr.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
